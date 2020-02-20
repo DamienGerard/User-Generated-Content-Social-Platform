397e02b5a57f8eb52718add418d203d2608e33ae
nicolasmelanie <nm>
2020-02-20 17:33:16 +0400

    changed

--- video_main.php
+++ video_main.php
@@ -6,40 +6,68 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Videos</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <title>Home</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://kit.fontawesome.com/e279e577b6.js"></script>
 
+    <script>
+var user_id = <?php echo $account->getId(); ?>
+
+$(document).ready(function(){
+    $("button.reportContent").click(function(){
+        var content_id = $(this).attr('id');
+        $.ajax({
+            url:"processReport.php",
+            data:{content_id: content_id,user_id: user_id},
+            cache: false,
+            method: "POST",
+            success: function(result){
+                $("div#abcd").html(result);
+            }
+        })
+
+    });
+});
+
+</script>
+
 </head>
 <body>
-    
-    <?php include 'navbar.php';?>
+<?php 
 
+        include 'navbar.php';
+
+    ?>
+    
     <div class="super-main">
 
-        <?php include 'side_column.php';?>
+    <?php 
+        if($type == "user"){
+            include 'side_column.php';
+        }
+        if($type == "admin"){
+            include 'adminSide_column.php';
+        }
+        ?>
 
         <div class="main">
-        <div class="generic-btn" style="display:block; width:80px; margin-bottom: 5px;"><a href="video_main.php" class="anchor-list-item"> <h3 style="margin: 0; padding: 0;">Videos</h3> </a></div>
+            <div class="generic-btn" style="display:block; width:75px"><a href="image_main.php" class="anchor-list-item"> <h3>Videos</h3> </a></div>
                 <?php
                     $query = 'SELECT * FROM webprojectdatabase.video INNER JOIN webprojectdatabase.content ON content.content_id = video.video_id INNER JOIN webprojectdatabase.user ON user.user_id = content.user_id ORDER BY content.date DESC';
                     $res = $pdo->prepare($query);
                     $res->execute();
+                    echo '<div id="abcd"></div>';
                     while($row = $res->fetch()){
-                        echo '<div style="background-color:lightgrey; border-radius: 5px; border: 3px solid black; height:350px; overflow:hidden">';
+                        echo '<div style="background-color:lightgrey; border-radius: 5px; border: 3px solid black; height:350px; overflow:hidden" class="articleDisplayed" id="'.$row['content_id'].'">';
                         echo '<table>';
                         echo '<tr>';
-                        echo '<td colspan="2">'."<a class=\"generic-btn anchor-list-item\" href='video.php?content_id=".$row['content_id']."'>".$row['title']."</a>".'</td>';
+                        echo '<td colspan="2">'."<a class=\"generic-btn anchor-list-item\" href='video.php?content_id=".$row['content_id']."'>".$row['title']."</a>".$row['content_id'].'<button class="reportContent" type="button" id="'.$row['content_id'].'" style="float:right; border-radius:20px;font-size:15px;">x</button>'.'</td>';
                         echo '</tr>';
                         echo '<tr>';
                         echo '<td rowspan="2"><a href="user_profile.php?user='.$row['user_name'].'" class="generic-btn"><img src="'.$row['profile_pic'].'" alt="" style="height:50px"></a></td>';
-                        echo '<td><a class="generic-btn anchor-list-item" href="user_profile.php?user='.$row['user_name'].'"><p style="margin: 0px;">'.$row['f_name'].' '.$row['l_name'].'</p></a></td>';
+                        echo '<td><a class="generic-btn anchor-list-item" href="user_profile.php?user='.$row['user_name'].'"><p>'.$row['f_name'].' '.$row['l_name'].'</p></a></td>';
                         echo '</tr>';
                         echo '<tr>';
                         echo "<td>".$row['date']."</td>";
